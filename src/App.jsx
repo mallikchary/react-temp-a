3e1a54817fda6eb16d3cc54ac1d32029647a60f0
shekar345 <mallikchary2775@gmail.com>
2025-03-20 11:46:57 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,30 @@
-import { useState } from "react";
-import "./App.css";
-import { BrowserRouter, Link,Routes,Route } from "react-router-dom";
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+import { BrowserRouter,Link,Routes,Route } from 'react-router-dom';
 import App1 from "./components/App1";
 import App2 from "./components/App2";
+import App3 from "./components/App3";
+
 function App() {
   return (
     <BrowserRouter>
-      <div>
-        <Link to="app1">App1</Link>
-        <Link to="app2">App2</Link>
-      </div>
-      <hr></hr>
-      <div>
-        <Routes>
-          <Route index element={<App1 />} />
-          <Route path="app1" element={<App1 />} />
-          <Route path="app2" element={<App2 />} />
-        </Routes>
-      </div>
+    <div>
+      <Link to="app1">App1</Link>
+      <Link to="app2">App2</Link>
+      <Link to="app3">App3</Link>
+    </div>
+    <hr></hr>
+    <div>
+      <Routes>
+        <Route index element={<App1/>}></Route>
+        <Route path="app1" element={<App1/>}></Route>
+        <Route path="app2" element={<App2/>}></Route>
+        <Route path="app3" element={<App3/>}></Route>
+      </Routes>
+    </div>
     </BrowserRouter>
   );
 }
-export default App;
+export default App
--- src/components/App1.jsx
+++ src/components/App1.jsx
@@ -1,3 +1,8 @@
 export default function App1(){
-    return <h1>This is App1</h1>
+    return (
+        <div>
+        <h1>This is App1</h1>
+        <p style={{backgroundColor:'pink'}}>This is a paragraph</p>
+        </div>
+    )
 }
