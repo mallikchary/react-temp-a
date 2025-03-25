38d83e7354251dfd43e04691d80715bfc2b3e022
shekar345 <mallikchary2775@gmail.com>
2025-03-25 15:06:43 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,20 @@ import App3 from "./components/App3";
 import App4 from "./components/App4";
 import App5 from "./components/App5"
 import App6 from "./components/App6";
+import App7 from "./components/App7";
+import App8 from "./components/App8";
 function App() {
   return (
     <BrowserRouter>
       <div>
-        <Link to="app1">App1</Link>
-        <Link to="app2">App2</Link>
-        <Link to="app3">App3</Link>
-        <Link to="app4">App4</Link>
-        <Link to="app5">App5</Link>
-        <Link to="app6">App6</Link>
+        <Link to="app1">App1</Link>-
+        <Link to="app2">App2</Link>-
+        <Link to="app3">App3</Link>-
+        <Link to="app4">App4</Link>-
+        <Link to="app5">App5</Link>-
+        <Link to="app6">App6</Link>-
+        <Link to="app7">App7</Link>-
+        <Link to="app8">App8</Link>
       </div>
       <hr></hr>
       <div>
@@ -28,6 +32,8 @@ function App() {
           <Route path="app4" element={<App4 />} />
           <Route path="app5" element={<App5 />} />
           <Route path="app6" element={<App6 />} />
+          <Route path="app7" element={<App7 />} />
+          <Route path="app8" element={<App8 />} />
         </Routes>
       </div>
     </BrowserRouter>
